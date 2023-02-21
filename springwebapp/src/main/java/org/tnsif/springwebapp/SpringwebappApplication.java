1fc130e2b0d6a8a13a1e9b136346b6ce6ef98d08
Nithe <Nithe@scorpion>
2023-02-21 19:24:48 +0530

    first commit

--- springwebapp/src/main/java/org/tnsif/springwebapp/SpringwebappApplication.java
+++ springwebapp/src/main/java/org/tnsif/springwebapp/SpringwebappApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ComponentScan(basePackages="org.tnsif.srpingwebapp, + "org.tnsif.srpingwebapp.controller")
+
 public class SpringwebappApplication {
 
 	public static void main(String[] args) {
